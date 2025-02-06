c447db5b82b1c3d400504030803319943f0e929c
nkemissi <nkemissi.pro@hotmail.com>
2025-02-06 18:13:17 +0100

    exos 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,21 @@
-// On importe un plugin Webpack qui permet de générer automatiquement
-// un fichier HTML incluant nos fichiers JavaScript
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-// On exporte la configuration de Webpack
+
 module.exports = {
-  // On définit le mode 'development' qui permet d'avoir un code plus lisible
-  // et plus facile à déboguer (contrairement au mode 'production' qui optimise le code)
   mode: "development",
-  // Configuration du serveur de développement
   devServer: {
-    // Le serveur sera accessible sur le port 8081
-    // (exemple: http://localhost:8081)
-    port: 8081,
+    port: 8080,
   },
-  // Liste des plugins que Webpack va utiliser
   plugins: [
-    // On configure le plugin HtmlWebpackPlugin
-    new HtmlWebpackPlugin({
-      // Il va utiliser le fichier ./public/index.html comme modèle
-      // et y injecter automatiquement les scripts JavaScript générés
-      template: "./public/index.html",
-    }),
     new ModuleFederationPlugin({
-      name: "container", // nom unique du remote
+      name: "container",
       remotes: {
-        products: "products@http://localhost:8080/remoteEntry.js",
+        products: "products@http://localhost:8081/remoteEntry.js",
+        cart: "cart@http://localhost:8082/remoteEntry.js",
       },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
     }),
   ],
 };
